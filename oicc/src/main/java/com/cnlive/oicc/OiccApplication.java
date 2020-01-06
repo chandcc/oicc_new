0b6b0c87dae290baf4b0dae8efce25c6f3f29b6e
chenhao <15532946937@163.com>
2020-01-06 15:13:21 +0800

    项目初始化

---
+++ oicc/src/main/java/com/cnlive/oicc/OiccApplication.java
@@ -0,0 +1,13 @@
+package com.cnlive.oicc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OiccApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OiccApplication.class, args);
+    }
+
+}
